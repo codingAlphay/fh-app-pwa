10ca8dea06a1cf58638a6eaf5673bda36819f794
jesselang <jesse.lang@casablanca.at>
2023-05-02 18:54:53 +0200

    api endpoint infoboard working

--- fh-app-pwa/src/pages/components/Calendar.js
+++ fh-app-pwa/src/pages/components/Calendar.js
@@ -8,7 +8,7 @@ function Calendar(props) {
   
   useEffect(() => {
     async function fetchData() {
-      const newData = await handler("infoscreen/getklausureninrange?from=01.03.2023&until=30.07.2023");
+      const newData = await handler("infoboard/getrpdata?from=02.05.2023&until=15.07.2023&pkz=2110653142");
       setData(newData);
     }
     fetchData();
@@ -20,8 +20,9 @@ function Calendar(props) {
             {
                 data.map((item) => (
                     <div>
-                        <h1>{item.tag}</h1>
-                        <p>{item.name}</p>
+                        <h1>{item.courseName}</h1>
+                        <p>{item.lecturer}</p>
+                        <p>{item.date}</p>
                     </div>
                 ))
             }
