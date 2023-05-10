7612991544b283cb76c7e2a361fef4ade82ebe40
jesselang <jesse.lang@casablanca.at>
2023-05-10 19:53:07 +0200

    manifest adjusted

--- fh-app-pwa/src/pages/_app.js
+++ fh-app-pwa/src/pages/_app.js
@@ -55,18 +55,18 @@ export default function App({ Component, pageProps, props }) {
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/favicon/favicon-16x16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
           />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/favicon/favicon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
           />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <link rel="apple-touch-icon" href="/icons/favicon/mstile-70x70.png"></link>
         <meta name="theme-color" content="#008E4D" />
       </Head>
       <ToastContainer />
